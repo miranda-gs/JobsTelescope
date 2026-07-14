f23e404587d5cc747fdec5e40ad65ca5cef87e21
miranda-gs <gabrielmiranda61637@gmail.com>
2026-07-14 10:32:36 -0300

    Spring Initializr

---
+++ src/main/java/com/miranda_gs/JobsTelescope/JobsTelescopeApplication.java
@@ -0,0 +1,13 @@
+package com.miranda_gs.JobsTelescope;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobsTelescopeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobsTelescopeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/miranda_gs/JobsTelescope/JobsTelescopeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miranda_gs.JobsTelescope;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobsTelescopeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
