8c720ba0b07a47a9ccbc92429ad8b4523913b7a4
miranda-gs <gabrielmiranda61637@gmail.com>
2026-07-14 22:18:18 -0300

    Remove CommandLineRunner implementation from main class

--- src/main/java/com/miranda_gs/JobsTelescope/JobsTelescopeApplication.java
+++ src/main/java/com/miranda_gs/JobsTelescope/JobsTelescopeApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class JobsTelescopeApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(JobsTelescopeApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(JobsTelescopeApplication.class, args);
+    }
 }
